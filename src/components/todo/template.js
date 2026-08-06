e166d0245053e36ea47d16fb5bae4bcea54fe464
olivier blanche <olivierblanche@gmail.com>
2026-08-06 11:39:39 +0200

    integration des templates components

--- src/components/todo/template.js
+++ src/components/todo/template.js
@@ -1,8 +1,13 @@
-import './styles.css';
+import "./styles.css";
 
-
-export default function getTemplate(todo){
-    return `
-        <li>${todo.content}</li>
+export default function getTemplate(todo) {
+return `
+        <li data-id="${todo.id}" class="${todo.completed ? "completed" : ""}">
+            <div class="view"><input class="toggle" type="checkbox" ${todo.completed ? "checked" : ""}>
+                <label>${todo.content}</label>
+                    <button class="destroy"></button>
+            </div>
+            <input type="text" class="edit" value="${todo.content}">
+        </li>
         `;
 }
